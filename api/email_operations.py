ebaef09105523aeb06a58877940c73cbeeda4075
pydlv <pydlv64@gmail.com>
2020-09-17 19:58:38 -0500

    don't fail on failure to send verification email

--- api/email_operations.py
+++ api/email_operations.py
@@ -31,8 +31,11 @@ def send_verification_email(user: User):
     )
 
     if not settings.RUNNING_DEVSERVER:
-        sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
-        sg.send(message)
+        try:
+            sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
+            sg.send(message)
+        except:
+            logging.exception("Failed to send verification email.")
     else:
         print("Sent!", token.token)
 
@@ -67,6 +70,6 @@ def send_password_reset(user: User):
             sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
             sg.send(message)
         except:
-            logging.exception("Failed to send verification email.")
+            logging.exception("Failed to send password reset email.")
     else:
         print("Sent!", token.token)
