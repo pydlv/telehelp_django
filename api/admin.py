561782f5d2d3b9462d905c899c092783bdf0ea5f
pydlv <pydlv64@gmail.com>
2020-09-17 19:52:39 -0500

    add availbilityschedule and appointments to admin

--- api/admin.py
+++ api/admin.py
@@ -1,11 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import User
+from api.models import User, AvailabilitySchedule, Appointment
 
 
 class UserAdmin(admin.ModelAdmin):
     pass
 
 
+class AvailabilityScheduleAdmin(admin.ModelAdmin):
+    pass
+
+
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(User, UserAdmin)
+admin.site.register(AvailabilitySchedule, AvailabilityScheduleAdmin)
+admin.site.register(Appointment, AppointmentAdmin)
