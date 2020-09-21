b4bfaa95e65ed0c03569021b6e4d5b70f666f53e
pydlv <pydlv64@gmail.com>
2020-09-21 18:23:06 -0500

    Appointment request in admin

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from api.models import User, AvailabilitySchedule, Appointment
+from api.models import User, AvailabilitySchedule, Appointment, AppointmentRequest
 
 
 class UserAdmin(admin.ModelAdmin):
@@ -16,6 +16,11 @@ class AppointmentAdmin(admin.ModelAdmin):
     pass
 
 
+class AppointmentRequestAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(AvailabilitySchedule, AvailabilityScheduleAdmin)
 admin.site.register(Appointment, AppointmentAdmin)
+admin.site.register(AppointmentRequest, AppointmentRequestAdmin)
